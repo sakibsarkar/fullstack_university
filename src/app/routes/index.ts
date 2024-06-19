884eafa17f1dbb395f27fb0510df5f470ce5f93c
sakibsarkar <sakibsarkar707@gmail.com>
2024-06-19 20:10:29 +0600

    enrolled course route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { AuthRoutes } from "../modules/Auth/auth.route";
+import { offeredCourseRoutes } from "../modules/OfferedCourse/OfferedCourse.route";
 import academicDepertmentRoutes from "../modules/academicDepartment/academicDepartment.route";
 import academicFacultyRoutes from "../modules/academicFaculty/academicFaculty.route";
 import academicSemesterRoutes from "../modules/academicSemester/academicSemester.route";
 import adminRoutes from "../modules/admin/admin.route";
 import courseRoutes from "../modules/course/course.route";
+import { EnrolledCourseRoutes } from "../modules/enrolledCourse/enrolledCourse.route";
 import facultyRoutes from "../modules/faculty/faculty.route";
 import semesterRegistrationRoutes from "../modules/semesterRegistration/semesterRegistration.route";
 import studentRoutes from "../modules/student/studen.route";
 import userRoutes from "../modules/user/user.route";
-import { offeredCourseRoutes } from "../modules/OfferedCourse/OfferedCourse.route";
 const router = express.Router();
 
 const moduleRoute = [
@@ -57,6 +58,10 @@ const moduleRoute = [
   {
     path: "/auth",
     route: AuthRoutes,
+  },
+  {
+    path: "/enrolled-courses",
+    route: EnrolledCourseRoutes,
   },
 ];
 
